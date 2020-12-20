6717e8b59793777f46b8e8eec4564a6ac47d8f3c
Jafer Alhaboubi <habboubi@gmail.com>
2020-12-20 15:16:12 -0600

    Quizzes add

---
+++ Quiz/DictReader_.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+    
+import os
+import csv
+
+"""
+    Using the CSV file of flowers again, fill in the gaps of the contents_of_file function 
+    to process the data without turning it into a dictionary. How do you skip over the header
+    record with the field names?
+"""
+
+# Create a file with data in it
+def create_file(filename):
+  with open(filename, "w") as file:
+    file.write("name,color,type\n")
+    file.write("carnation,pink,annual\n")
+    file.write("daffodil,yellow,perennial\n")
+    file.write("iris,blue,perennial\n")
+    file.write("poinsettia,red,perennial\n")
+    file.write("sunflower,yellow,annual\n")
+
+# Read the file contents and format the information about each row
+def contents_of_file(filename):
+  return_string = ""
+
+  # Call the function to create the file 
+  create_file(filename)
+
+  # Open the file
+  ___
+    # Read the rows of the file
+    rows = ___
+    # Process each row
+    for row in rows:
+      ___ = row
+      # Format the return string for data rows only
+
+          return_string += "a {} {} is {}\n".format(___)
+  return return_string
+
+#Call the function
+print(contents_of_file("flowers.csv"))
