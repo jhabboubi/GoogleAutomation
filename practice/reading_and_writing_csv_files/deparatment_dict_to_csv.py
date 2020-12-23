f4aeffbc5593304307f48ec364267cd02308998c
Jafer Alhaboubi <habboubi@gmail.com>
2020-12-23 13:14:01 -0600

    adding commits.

--- practice/reading_and_writing_csv_files/deparatment_dict_to_csv.py
+++ practice/reading_and_writing_csv_files/deparatment_dict_to_csv.py
@@ -5,7 +5,7 @@ users = [{"name": "Sol Mansi","username":"solm","department":"IT infrastructure"
 {"name": "Charlie Gray","username":"greyc","department":"Development"}]
 
 keys = ["name","username","department"]
-
+#open file csv and reading content using fields keys
 with open('by_department.csv', 'w') as by_department:
     writer= csv.DictWriter(by_department,fieldnames=keys)
     writer.writeheader()
