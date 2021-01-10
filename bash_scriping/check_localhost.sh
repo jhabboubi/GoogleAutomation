2ee5f6f74bb7cad3fda6065f9c04bd2ac894864e
Jafer Alhaboubi <habboubi@gmail.com>
2021-01-09 20:57:27 -0600

    added comments explaining the script

--- bash_scriping/check_localhost.sh
+++ bash_scriping/check_localhost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+#the script is going to check if localhost exist on the machine 
 if grep "127.0.0.1" /etc/hosts; then
     echo "Everything OK!"
 else "Error: 127.0.0.1 not found!"
